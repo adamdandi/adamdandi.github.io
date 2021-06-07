fed0f587593fca06b50f39f7bc02c1906659be7b
adamdandi <84269054+adamdandi@users.noreply.github.com>
2021-06-07 15:49:10 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ submitBtn.addEventListener("click", function(){
   progressText[current - 1].classList.add("active");
   current += 1;
   setTimeout(function(){
-    alert("Your Form Successfully Signed up");
+    alert("Semoga kita dipertemukan lagii ^.^");
     location.reload();
   },800);
 });
